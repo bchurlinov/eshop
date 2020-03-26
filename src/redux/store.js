b640615091d83a4d95398897156288bf9d2806a9
bchurlinov <bchurlinov@hotmail.com>
2020-03-26 18:37:30 +0100

    redux devtools production

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,12 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [];
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
+const devTools =
+  process.env.NODE_ENV === "production"
+    ? applyMiddleware(...middlewares)
+    : composeWithDevTools(applyMiddleware(...middlewares));
+
+export const store = createStore(rootReducer, devTools);
 export const persistor = persistStore(store);
 
 // Persist Redux - configuration in store.js, root-reducer.js and index.js
